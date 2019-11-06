90a698ba97128c4903c9a98d19819de2e2f77cbd
David Lee <davidslee101@gmail.com>
2019-11-06 18:58:50 -0500

    made the login page, sign up page, forgot password page look a little nicer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates: fullname, presence: true, length: {maximum: 50}
+  validates :fullname, presence: true, length: {maximum: 50}
+
 end

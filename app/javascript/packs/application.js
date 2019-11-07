392acf7f654449fba2550dbee094cf69918880f6
David Lee <davidslee101@gmail.com>
2019-11-07 16:03:49 -0500

    added some files to my gitignore so that I can set up email configurations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+//= require toastr
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,4 +16,15 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap/dist/js/bootstrap';
+
+
+
+// import toastr from 'toastr';
+
+// toastr.options = {
+//     "closeButton": true
+//     .... add options here ...
+// };
+
+// global.toastr = toastr;
 

ef1ea661d9d0bb1d0008a55b32f11bf63a9178ed
David Lee <davidslee101@gmail.com>
2019-11-09 00:21:31 -0500

    OMG it works. Now i'm just removing the hash rockets cause i hate them. BUT I CAN'T BELIEVE I FUCKING FIGURED IT OUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = 'https://squealer-david-lee.herokuapp.com'
   
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://squealer-david-lee.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'https://squealer-david-lee.herokuapp.com',
+    address:'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.

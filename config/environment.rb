f53b548c990e1fa62ca84447c7b6e4163a9bfbb9
David Lee <davidslee101@gmail.com>
2019-11-08 23:56:46 -0500

    dear god please work fucking rails 6

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
-config.action_mailer.default_url_options = { :host => 'https://squealer-david-lee.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
@@ -13,3 +10,5 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+# Initialize the Rails application.
+Rails.application.initialize!

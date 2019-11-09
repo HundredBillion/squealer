095891a1533e09c908201d2e4e4ad564e76a1088
David Lee <davidslee101@gmail.com>
2019-11-08 23:50:26 -0500

    trying another thing... fucking Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,11 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { :host => 'https://squealer-david-lee.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'https://squealer-david-lee.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
